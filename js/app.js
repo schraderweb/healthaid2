b31ffda1eaaf33ef5615790c06a3ce063092a1e3
Andrew <86363077+catc24@users.noreply.github.com>
2024-07-08 20:36:23 -0400

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,9 +5,12 @@ const q2BtnNo = document.querySelector(".q2-btn-no");
 const loading3 = document.getElementById("loading3");
 const loading2 = document.getElementById("loading2");
 const loading1 = document.getElementById("loading1");
+const qualifyDiv = document.getElementById("qualify");
+const dontQualifyDiv = document.getElementById("dontqualify");
 
 const q2Btn = document.querySelector("#q2");
 const q1Btn = document.querySelector("#q1");
+
 let choiceUnder = true;
 let choiceMedical = true;
 
@@ -37,98 +40,63 @@ function startTimer() {
 }
 
 const showText4 = () => {
-    if(choiceUnder == true  ) {
-        var timerInterval = setInterval(startTimer, 1000);
-        document.getElementById("qualify").style.display = "block";
-    }else {
-        document.getElementById("dontqualify").style.display = "block";
-    }
-}
+  if (choiceUnder) {
+    var timerInterval = setInterval(startTimer, 1000);
+    qualifyDiv.style.display = "block";
+  } else {
+    dontQualifyDiv.style.display = "block";
+  }
+};
+
 const showText3 = () => {
-    loading3.style.display = "block";
-    setTimeout(() => {
-        console.log('Foo bar1');
-        loading3.style.display = "none";
-        showText4();
-      }, 1000)
-}
+  loading3.style.display = "block";
+  setTimeout(() => {
+    loading3.style.display = "none";
+    showText4();
+  }, 1000);
+};
+
 const showText2 = () => {
-    loading2.style.display = "block";
-    setTimeout(() => {
-        console.log('Foo bar1');
-        loading2.style.display = "none";
-        showText3();
-      }, 1000)
-}
+  loading2.style.display = "block";
+  setTimeout(() => {
+    loading2.style.display = "none";
+    showText3();
+  }, 1000);
+};
+
 const showText1 = () => {
-    loading1.style.display = "block";
-    setTimeout(() => {
-        console.log('Foo bar');
-        loading1.style.display = "none";
-        showText2();
-      }, 1000)
-}
+  loading1.style.display = "block";
+  setTimeout(() => {
+    loading1.style.display = "none";
+    showText2();
+  }, 1000);
+};
 
 q1BtnYes.addEventListener("click", () => {
-    q1Btn.style.display = "none";
-    q2Btn.style.display = "none";
-      loading1.classList.add("fade-out");
-    choiceUnder = false;
-    showText1();
+  q1Btn.style.display = "none";
+  q2Btn.style.display = "none";
+  loading1.classList.add("fade-out");
+  choiceUnder = true;
+  showText1();
 });
+
 q1BtnNo.addEventListener("click", () => {
-    q1Btn.style.display = "none";
-    q2Btn.style.display = "none";
-    q2Btn.classList.add("fade-in");
-    loading1.classList.add("fade-out");
-    choiceUnder = false;
-    showText1();
+  q1Btn.style.display = "none";
+  q2Btn.style.display = "none";
+  q2Btn.classList.add("fade-in");
+  loading1.classList.add("fade-out");
+  choiceUnder = false;
+  showText1();
 });
 
 q2BtnYes.addEventListener("click", () => {
-    q2Btn.style.display = "none";
-    choiceMedical = false;
-    choiceUnder = false
-    showText1();
+  q2Btn.style.display = "none";
+  choiceMedical = true;
+  showText1();
 });
+
 q2BtnNo.addEventListener("click", () => {
-    q2Btn.style.display = "none";
-    choiceMedical = false;
-    showText1();
+  q2Btn.style.display = "none";
+  choiceMedical = false;
+  showText1();
 });
-
-
-
-
-/*  Original JS
-
-q1BtnYes.addEventListener("click", () => {
-    q1Btn.style.display = "none";
-    q2Btn.style.display = "block";
-    q1Btn.classList.add("fade-in");
-    q2Btn.classList.add("fade-out");
-   choiceUnder = true;
-});
-q1BtnNo.addEventListener("click", () => {
-    q1Btn.style.display = "none";
-    q2Btn.style.display = "none";
-    q2Btn.classList.add("fade-in");
-    loading1.classList.add("fade-out");
-    choiceUnder = false;
-    showText1();
-});
-
-q2BtnYes.addEventListener("click", () => {
-    q2Btn.style.display = "none";
-    choiceMedical = false;
-    choiceUnder = false
-    showText1();
-});
-q2BtnNo.addEventListener("click", () => {
-    q2Btn.style.display = "none";
-    choiceMedical = false;
-    showText1();
-});
-
-
-//
